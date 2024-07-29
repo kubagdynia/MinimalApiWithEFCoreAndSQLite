ba65b6025ad738cdfca9010cf6c465a9a81232d1
kubagdynia <kubagdynia@gmail.com>
2024-07-29 12:44:22 +0200

    Added new columns to the rpg character

---
+++ Api/Migrations/20240729102937_AddNewColumnsToRpgCharacter.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewColumnsToRpgCharacter : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Defense",
+                table: "RpgCharacters",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Strength",
+                table: "RpgCharacters",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Weapon",
+                table: "RpgCharacters",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Defense",
+                table: "RpgCharacters");
+
+            migrationBuilder.DropColumn(
+                name: "Strength",
+                table: "RpgCharacters");
+
+            migrationBuilder.DropColumn(
+                name: "Weapon",
+                table: "RpgCharacters");
+        }
+    }
+}
--- Api/RpgCharacter.cs
+++ Api/RpgCharacter.cs
@@ -6,4 +6,7 @@ public record RpgCharacter
     public string Name { get; set; } = string.Empty;
     public string RpgClass { get; set; } = string.Empty;
     public int HitPoints { get; set; } = 100;
+    public string Weapon { get; set; } = string.Empty;
+    public int Strength { get; set; } = 10;
+    public int Defense { get; set; } = 10;
 }
