acd9e3c2d213b9ae754f70aa869653377c18dca0
kubagdynia <kubagdynia@gmail.com>
2025-04-08 22:09:26 +0200

    Update libs

--- Api/RpgEndpoints.cs
+++ Api/RpgEndpoints.cs
@@ -28,45 +28,42 @@ public static class RpgEndpoints
         DeleteRpgCharacter(rpgCharacterRoute);
     }
 
+    // POST /api/rpgCharacters
     private static void CreateNewRpgCharacter(RouteGroupBuilder rpgCharacterRoute)
-    {
-        // POST /api/rpgCharacters
-        rpgCharacterRoute.MapPost("", async (DataContext dataContext, RpgCharacter rpgCharacter) =>
+        => rpgCharacterRoute.MapPost("", async (DataContext dataContext, RpgCharacter rpgCharacter) =>
             {
                 dataContext.RpgCharacters.Add(rpgCharacter);
                 await dataContext.SaveChangesAsync();
                 return Results.Created($"/api/rpgCharacters/{rpgCharacter.Id}", rpgCharacter);
             })
             .WithSummary("Creates a new RpgCharacter")
+            .WithDescription("Creates a new RPG character with the provided details.")
             .Produces<Created>(StatusCodes.Status201Created);
-    }
+            
+            
 
+    // GET /api/rpgCharacters
     private static void GetAllRpgCharacters(RouteGroupBuilder rpgCharacterRoute)
-    {
-        // GET /api/rpgCharacters
-        rpgCharacterRoute.MapGet("",
+        => rpgCharacterRoute.MapGet("",
                 async (DataContext dataContext) => Results.Ok(await dataContext.RpgCharacters.ToListAsync()))
             .WithSummary("Gets all RpgCharacters")
             .Produces<Ok>();
-    }
 
+    // GET /api/rpgCharacters/{id}
     private static void GetRpgCharacter(RouteGroupBuilder rpgCharacterRoute)
-    {
-        // GET /api/rpgCharacters/{id}
-        rpgCharacterRoute.MapGet("{id:int}", async (DataContext dataContext, int id) =>
+        => rpgCharacterRoute.MapGet("{id:int}", async (DataContext dataContext, int id) =>
             {
                 var rpgCharacter = await dataContext.RpgCharacters.FindAsync(id);
                 return rpgCharacter is null ? Results.NotFound() : Results.Ok(rpgCharacter);
             })
             .WithSummary("Gets a specific RpgCharacter")
+            .WithDescription("Gets the details of a specific RPG character by ID.")
             .Produces<Ok>()
             .Produces<NotFound>(StatusCodes.Status404NotFound);
-    }
 
+    // PUT /api/rpgCharacters/{id}
     private static void UpdateRpgCharacter(RouteGroupBuilder rpgCharacterRoute)
-    {
-        // PUT /api/rpgCharacters/{id}
-        rpgCharacterRoute.MapPut("{id:int}", async (DataContext dataContext, int id, RpgCharacter rpgCharacter) =>
+        => rpgCharacterRoute.MapPut("{id:int}", async (DataContext dataContext, int id, RpgCharacter rpgCharacter) =>
             {
                 if (id != rpgCharacter.Id)
                 {
@@ -95,26 +92,23 @@ public static class RpgEndpoints
             .Produces<NoContent>(StatusCodes.Status204NoContent)
             .Produces<NotFound>(StatusCodes.Status400BadRequest)
             .Produces<NotFound>(StatusCodes.Status404NotFound);
-    }
-
+    
+    // DELETE /api/rpgCharacters/{id}
     private static void DeleteRpgCharacter(RouteGroupBuilder rpgCharacterRoute)
-    {
-        // DELETE /api/rpgCharacters/{id}
-        rpgCharacterRoute.MapDelete("{id:int}", async (DataContext dataContext, int id) =>
+        => rpgCharacterRoute.MapDelete("{id:int}", async (DataContext dataContext, int id) =>
             {
                 var rpgCharacter = await dataContext.RpgCharacters.FindAsync(id);
                 if (rpgCharacter is null)
                 {
                     return Results.NotFound();
                 }
-        
+
                 dataContext.RpgCharacters.Remove(rpgCharacter);
                 await dataContext.SaveChangesAsync();
-        
+
                 return Results.NoContent();
             })
             .WithSummary("Deletes a specific RpgCharacter")
             .Produces<NoContent>(StatusCodes.Status204NoContent)
             .Produces<NotFound>(StatusCodes.Status404NotFound);
-    }
 }
